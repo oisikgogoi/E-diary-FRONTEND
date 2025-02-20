1da60501b33239584a0670bfda22a29107220bff
Oisik <98504957+oisikgogoi@users.noreply.github.com>
2025-02-21 00:18:59 +0530

    Update EditNote.js

--- src/components/EditNote.js
+++ src/components/EditNote.js
@@ -32,7 +32,7 @@ function EditNote() {
         };
   
         try {
-          const response = await axios.get(`/api/note/${params.id}`,config);
+          const response = await axios.get(`https://e-diary-backend-oisik-clt8.onrender.com/api/note/${params.id}`,config);
           setTitle(response.data.title)
           setDescription(response.data.description)
 
